9d6a7cf169b7264b06af9ec8ee5076ab2c6b0b79
hameedfawwaz <fawwazgithub@gmail.com>
2023-12-31 12:05:48 -0700

    adding files to repo

---
+++ main.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from pathlib import Path as path
+from moviepy.editor import VideoFileClip
+from processes.process import process
+
+st.title("Deep Fake Generator")
+st.header("Create deep faked videos from images using templates!")
+
+st.write("Welcome to the deep fake generator! Here are the instructions to use the deep fake generator!\n 1. Upload the image you want to deep fake! (accepted formats: PNG, JPG)\n 2. Upload the template video you would like to use, it must be less than 30 seconds in length (accepted format: MP4)\n 3. Press submit and please wait up to 20 minutes for your request to process and do not leave the application!")
+
+with st.form(key="Form :", clear_on_submit= True):
+
+    img = st.file_uploader(label="upload the image you want to deepfake")
+    temp = st.file_uploader(label="upload the template you would like to use")
+    submit = st.form_submit_button(label="Submit")
+
+if submit:
+
+    st.write("Processing... Please allow up to 15 minutes for your request to process.")
+    img_path = path("./saved", img.name)
+    temp_path = path("./saved", temp.name)
+    with open(img_path, mode="wb") as w:
+        w.write(img.getvalue())
+    with open(temp_path, mode="wb") as w:
+        w.write(temp.getvalue())
+        
+
+    if img_path.exists() and temp_path.exists():  
+
+        len_vid = VideoFileClip(f"./saved/{temp.name}").duration
+        st.write("video length: " + str(len_vid))
+
+        if len_vid <= 30:
+            process.generate(img.name, temp.name)
+
+            img_name = img.name.partition(".")
+            video_file = open(f'./generated/{img_name}.mp4', "rb")
+            video_bytes = video_file.read()
+
+            st.video(video_bytes)
+        else:
+            st.write("Error! Make sure that your template video is less than 30 seconds in length!")
+
---
+++ processes/process.py
@@ -0,0 +1,55 @@
+import imageio
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from skimage.transform import resize
+from IPython.display import HTML
+from processes.demo import load_checkpoints, make_animation
+from skimage import img_as_ubyte
+from moviepy.editor import *
+from moviepy.editor import VideoFileClip
+import ffmpeg
+
+class process():
+
+    def generate(img, temp):
+        source_image = imageio.imread(f"./saved/{img}")
+
+        #unedited_vidclip = VideoFileClip(f"./template/{temp}")
+        
+
+        reader = imageio.get_reader(f"./saved/{temp}")
+        
+        
+        source_image = resize(source_image, (256, 256))[..., :3]
+
+        fps = reader.get_meta_data()['fps']
+        driving_video = []
+        try:
+            for im in reader:
+                driving_video.append(im)
+        except RuntimeError:
+            pass
+        reader.close()
+
+        
+        driving_video = [resize(frame, (256, 256))[..., :3] for frame in driving_video]
+
+        generator, kp_detector = load_checkpoints(config_path='./processes/config/vox-256.yaml', 
+                                checkpoint_path='./processes/config/vox-cpk.pth.tar')
+
+        predictions = make_animation(source_image, driving_video, generator, kp_detector, relative=True)
+
+        img = img.partition(".")
+
+        imageio.mimsave(f'./generated/{img}.mp4', [img_as_ubyte(frame) for frame in predictions], fps=fps)
+
+        predictions = make_animation(source_image, driving_video, generator, kp_detector, relative=False, adapt_movement_scale=True)
+
+        #vidclip = VideoFileClip(f"./template/")
+        #audclip = ffmpeg.input(f"./processes/template/{temp}.mp3")
+
+       # out = ffmpeg.output(vidclip, audclip, f"./generated/{img}2.mp4")
+        #out.run()
+
+    
